eae9ee93ff2c6e3bec0c0fe6f56427a43195fa14
Quentin <59879188+quentinDelaporte@users.noreply.github.com>
2022-02-08 16:51:54 +0100

    Implémentation du formulaire d'horaires

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -11,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
 class Client
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
+    #[ORM\GeneratedValue(strategy: "NONE")]
     #[ORM\Column(type: 'integer')]
     private $id;
 
