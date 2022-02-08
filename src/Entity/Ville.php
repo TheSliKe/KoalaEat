5b7a23075a361a983245b9769e288fc5984f919a
Julien <juju.dupale@gmail.com>
2022-02-08 15:10:44 +0100

    fully reabase

--- src/Entity/Ville.php
+++ src/Entity/Ville.php
@@ -122,12 +122,9 @@ class Ville
 
         return $this;
     }
-}
+
     public function __toString(){
         return $this->VI_Libelle;
     }
-}
-    public function __toString(){
-        return $this->VI_Libelle;
-    }
+
 }
