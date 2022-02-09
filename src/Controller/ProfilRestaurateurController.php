61060a909df1ae3b3e58bf86633a92f4ec983c4b
Quentin <59879188+quentinDelaporte@users.noreply.github.com>
2022-02-09 13:09:24 +0100

    form ajout plat

--- src/Controller/ProfilRestaurateurController.php
+++ src/Controller/ProfilRestaurateurController.php
@@ -13,7 +13,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\RestaurateurRepository;
 
 class ProfilRestaurateurController extends AbstractController
 {
