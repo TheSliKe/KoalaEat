25cea8b02f72f453b1c35316f7791a3c9dcf0e56
skalucska <skalucska@gmail.com>
2022-02-10 16:01:45 +0100

    dashbordLivreurAFinir

--- src/Controller/DashboardLivreurController.php
+++ src/Controller/DashboardLivreurController.php
@@ -19,38 +19,35 @@ class DashboardLivreurController extends AbstractController
     {
         $conn = $entityManager->getConnection();
         $sql = '
-            select c.id as id, 
+        select c.id as id, 
                 re.re_adresse as restaurantAdresse, 
                 po.po_date as dateCommande, 
                 c.co_adresse_de_livraison as livraisonAdresse
             FROM commande c
                 INNER JOIN possede po ON c.id = po.fk_co_id
-                LEFT JOIN compose co ON co.fk_co_id = c.id
-                LEFT JOIN plat pl ON pl.id = co.fk_pa_id
-                LEFT JOIN restaurant re ON re.id = pl.fk_re_id
-            where po.fk_st_id = 1; 
+                LEFT JOIN restaurant re ON re.id = c.fk_restaurant_id
+            where po.fk_st_id = 2 AND po.po_date in (select max(possede.po_date) from possede group by possede.fk_co_id); 
             ';
         $stmt = $conn->prepare($sql);
         $resultSet = $stmt->executeQuery();
         $result = $resultSet->fetchAllAssociative();
-        // $sql2 = '
-        //     select c.id as id, 
-        //         re.re_adresse as restaurantAdresse, 
-        //         po.po_date as dateCommande, 
-        //         c.co_adresse_de_livraison as livraisonAdresse
-        //     FROM commande c
-        //         INNER JOIN possede po ON c.id = po.fk_co_id
-        //         LEFT JOIN compose co ON co.fk_co_id = c.id
-        //         LEFT JOIN plat pl ON pl.id = co.fk_pa_id
-        //         LEFT JOIN restaurant re ON re.id = pl.fk_re_id
-        //     where po.fk_st_id = 1; 
-        //     ';
-        // $stmt2 = $conn2->prepare($sql);
-        // $resultSet2 = $stmt2->executeQuery();
-        // $result2 = $resultSet2->fetchAllAssociative();
+        $sql2 = '
+        select c.id as id, 
+        re.re_adresse as restaurantAdresse, 
+        po.po_date as dateCommande, 
+        c.co_adresse_de_livraison as livraisonAdresse
+    FROM commande c
+        INNER JOIN possede po ON c.id = po.fk_co_id
+        LEFT JOIN restaurant re ON re.id = c.fk_restaurant_id
+    where po.fk_st_id = 5 AND po.fk_st_id = 6 AND po.po_date in (select max(possede.po_date) from possede group by possede.fk_co_id);
+            ';
+        $stmt2 = $conn->prepare($sql2);
+        $resultSet2 = $stmt2->executeQuery();
+        $result2 = $resultSet2->fetchAllAssociative();
 
         return $this->render('dashboard_livreur/index.html.twig', [
-            'commandes' => $result
+            'commandes' => $result,
+            'commandesPreiseEnCharge' => $result2
         ]);
     }
 }
